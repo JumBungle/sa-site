f07328306161d961daaf025681b9d6d26c48d4d5
JumBungle <pshotstuff@gmail.com>
2023-09-12 15:30:21 +1200

    ContactForm

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 function ContactForm() {
-  return <div>ContactForm</div>;
+  const [formStatus, setFormStatus] = React.useState("Send");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setFormStatus("Submitting...");
+    const { name, email, message } = e.target.elements;
+    let conFom = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    console.log(conFom);
+  };
+
+  return (
+    <>
+      <div className="from-container">
+        <h2>Get in Touch</h2>
+        <form onSubmit={onSubmit}>
+          <div className="form-section">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" required />
+          </div>
+          <div className="form-section">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" required />
+          </div>
+          <div className="form-section">
+            <label htmlFor="subject">Subject:</label>
+            <input type="text" id="subject" required />
+          </div>
+          <div className="form-section">
+            <label htmlFor="message">Message:</label>
+            <input type="text" id="message" required />
+          </div>
+          <button type="submit">{formStatus}</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default ContactForm;
